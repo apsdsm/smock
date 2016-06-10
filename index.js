d948e374a57f79e56641a16482ca7bf1b60486d4
derupozo <derupozo@reas.jp>
2016-06-10 18:28:29 +0900

    first working release

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function(request,response){
-//   response.send('Base URL');
-// });
-
-// app.get('*', function(request,response){
-//   console.log(request.baseUrl);
-//   response.send(request.url);
-// });
-
-// app.listen(5555, function(){
-//   console.log('Listening on port 5555');
-// });
-// 
-
-
 var server = require('./lib/server');
 
-console.log('starting app...');
+console.log('starting smock...');
 
-server.start();
+server.start(__dirname);
 
-console.log('started...');
+console.log('started... Ctrl-C to exit');
--- test/serverTest.js
+++ test/serverTest.js
@@ -1,72 +1,89 @@
 var assert = require('chai').assert;
 var request = require('supertest');
 var mockfs = require('mock-fs');
-
 var server = require('../lib/server');
 
-describe('Server', function(){
 
-  before(function(){
-    var default_routes = {
-      'foobar': 'baz.json'
-    };
+describe('Server', function () {
 
-    var alternative_routes = {
-      'foobar': 'altbaz.json'
-    }
+  before(function () {
 
-    mockfs({
-      'routes': {
-        'default.json': default_routes,
-        'alternative.json': alternative_routes
-      },
-      'responses': {
-        'baz.json': "baz.json contents",
-        'altbaz.json': "altbaz.json contents"
-      }
-    });
+    var rootDir = __dirname+'/../';
 
     var config = {
       "host": "localhost",
-      "port": "5555",  
-      "routes": "routes",
-      "responses": "responses"
+      "port": "5555",
+      "routes": "test/fixtures/routes",
+      "responses": "test/fixtures/responses"
     }
 
-    server.start(config);
+    server.start(rootDir, config);
   });
 
-  after(function(){
+  after(function () {
     mockfs.restore();
   });
 
-  it('shows the active config', function(done){
+  it('shows the base page', function (done) {
+    request(server.app)
+      .get('/')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  })
+
+  it('shows the active config', function (done) {
     request(server.app)
       .get('/config')
       .set('Accept', 'application/json')
       .expect(200, {
         host: 'localhost',
         port: '5555',
-        routes: 'routes',
-        responses: 'responses'
+        routes: 'test/fixtures/routes',
+        responses: 'test/fixtures/responses'
       })
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err);
         done();
       });
   });
 
-  it('shows the active routes', function(done){
+  it('shows the active routes', function (done) {
     request(server.app)
       .get('/routes')
       .set('Accept', 'application/json')
       .expect(200, {
-        'foobar': 'baz.json'
+        '/foobar': 'default.json'
       })
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err);
         done();
       });
   });
+
+  it('returns the content of a requested file', function (done) {
+    request(server.app)
+      .get('/foobar')
+      .set('Accept', 'application/json')
+      .expect(200, {
+        content: "default content"
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('returns a 404 if there is no match for request', function (done) {
+    request(server.app)
+      .get('/not-found')
+      .expect(404)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  })
 
 });
